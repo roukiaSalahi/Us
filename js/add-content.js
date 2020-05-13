a1b2dcc2cc86fddc476c9af1f08a53c860543a8b
roukiaSalahi <roqaia.sa@hotmail.com>
2020-05-13 09:07:02 -0700

    js

--- js/add-content.js
+++ js/add-content.js
@@ -3,20 +3,21 @@ alert('Hello welcome to our page :)')
 var hourNow = prompt ( ' what time is it 0 - 23 ? ' ) ;
 console.log(hourNow)
 var greeting;
+var nice;
 if (hourNow > 18 && hourNow <= 24 ) {
     greeting ='Good Evening !';
+    nice = 'Have a nice day :)'
 } else if (hourNow > 12 && hourNow <24 ) {
     greeting ='Good Afternoon !';
+    nice = 'Have a nice day :)'
 } else if (hourNow >= 0 && hourNow <24 ) {
     greeting ='Good Morning!';
+    nice = 'Have a nice day :)'
 } else {
     greeting ='Something went wrong!';
 }
 document.write('<h3>'+ greeting +'</h3>');
-var nice;
-if (hourNow > 0 && hourNow <= 24){
-    nice = 'Have a nice day :)'
-}
+
 
 document.write('<h4>' + nice + '</h4>');
 
